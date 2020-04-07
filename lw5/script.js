c7b983690841c8a1afbb7ccd4ba57b466578a19d
mrjayse <43705694+mrjayse@users.noreply.github.com>
2020-04-07 18:12:20 +0300

    Add files via upload

--- lw5/script.js
+++ lw5/script.js
@@ -1,24 +1,24 @@
 //#region catVasiliy
 var catVasiliy = {
-    name: 'Василий',
-    birthday: new Date(),
-    listOfFears: ['vacuum cleaner', 'beep'],
-    listOfLove: ['ksksks'],
-    meow: function () {
-      console.log('Meow!');
-    },
-    reaction: function(feeling) {
-      if (this.listOfFears.indexOf(feeling) > -1) {
-        this.meow();
-        console.log('run from here!!!!')
-      }
-      else if (this.listOfLove.indexOf(feeling) > -1) {
-          console.log('purr');
-      }
-      else {
-          console.log('Meow?');
-      }
-    },
+  name: 'Василий',
+  birthday: new Date(),
+  listOfFears: ['vacuum cleaner', 'beep'],
+  listOfLove: ['ksksks'],
+  meow: function () {
+    console.log('Meow!');
+  },
+  reaction: function (feeling) {
+    if (this.listOfFears.indexOf(feeling) > -1) {
+      this.meow();
+      console.log('run from here!!!!')
+    }
+    else if (this.listOfLove.indexOf(feeling) > -1) {
+      console.log('purr');
+    }
+    else {
+      console.log('Meow?');
+    }
+  },
 };
 //#endregion
 
@@ -34,51 +34,36 @@ var refund = {
 }
 
 var cashbox = {
-    amount: 0,
-    addPayment: function(payment) {
-      if (payment.amount != 0) {
-        var paymentType;
-        if (payment.amount > 0) {
-          paymentType = "accrual";
-        }
-        else {
-          paymentType = "write-off";
-        }
-
-        switch (paymentType) {
-          case "accrual":
+  amount: 0,
+  addPayment: function (payment) {
+    if (!Number.isInteger(payment.amount)) {
+      console.log("payment.amount не является числом");
+    }
+    else {
+      if (payment.amount !== 0) {
             this.amount += payment.amount;
             console.log("Тип транзакции: Начисление");
             console.log("Сумма начисления: " + payment.amount);
             console.log("Комментарий: " + payment.info);
             console.log("Текущая сумма счета: " + this.amount);
-            break;
-          case "write-off":
-            if ((this.amount + payment.amount) >= 0) {
-              this.amount += payment.amount;
-              console.log("Тип транзакции: Списание");
-              console.log("Сумма списания: " + Math.abs(payment.amount));
-              console.log("Комментарий: " + payment.info);
-              console.log("Текущая сумма счета: " + this.amount);
-            }
-            else {
-              console.error("Ошибка: Недостаточно средств");
-            }
-            break;
-        }
       }
       else {
         console.error("Ошибка: Сумма не должна быть равна 0");
       }
-    },
-    refundPayment: function(refund) {
+    }
+  },
+  refundPayment: function (refund) {
+    if (!Number.isInteger(refund.amount)) {
+      console.log("refund.amount не является числом");
+    }
+    else {
       if (refund.amount >= 0) {
         if ((this.amount - refund.amount) >= 0) {
           this.amount -= refund.amount;
-            console.log("Тип транзакции: Возврат");
-            console.log("Сумма возврата: " + refund.amount);
-            console.log("Комментарий: " + refund.info);
-            console.log("Текущая сумма счета: " + this.amount);
+          console.log("Тип транзакции: Возврат");
+          console.log("Сумма возврата: " + refund.amount);
+          console.log("Комментарий: " + refund.info);
+          console.log("Текущая сумма счета: " + this.amount);
         }
         else {
           console.error("Ошибка: Недостаточно средств");
@@ -87,12 +72,13 @@ var cashbox = {
       else {
         console.error("Ошибка: Сумма должна быть больше 0");
       }
-    },
+    }
+  },
 };
 
 // cashbox.addPayment({ amount: -10, info: 'Оплата штрафа'}); // show error (console), amount not affected
 // cashbox.addPayment({  amount: 10,  info: 'Оплата ЖКХ' }); // cashbox amount = 10
-   
+
 // cashbox.refundPayment({  amount: 10,  info: 'Возврат клиенту' }); // cashbox amount = 0
 // cashbox.refundPayment({  amount: 10,  info: 'Возврат клиенту' }); // cashbox amount not affected (warning)
 //#endregion
@@ -138,22 +124,22 @@ var timeR2 = {
 function isTimeRangesIntersect(timeRange1, timeRange2) {
   timeR1.t1 = new Date("1970-01-01T" + timeRange1[0]);
   timeR1.t2 = new Date("1970-01-01T" + timeRange1[1]);
-  
+
   timeR2.t1 = new Date("1970-01-01T" + timeRange2[0]);
   timeR2.t2 = new Date("1970-01-01T" + timeRange2[1]);
 
   if (timeR1.t1 == "Invalid Date" ||
-      timeR1.t2 == "Invalid Date" ||
-      timeR2.t1 == "Invalid Date" ||
-      timeR2.t2 == "Invalid Date") {
+    timeR1.t2 == "Invalid Date" ||
+    timeR2.t1 == "Invalid Date" ||
+    timeR2.t2 == "Invalid Date") {
     console.error("Invalid date");
     return;
   }
 
   if ((timeR1.t2 > timeR2.t1 && timeR1.t2 < timeR2.t2) ||
-      (timeR1.t1 > timeR2.t1 && timeR1.t1 < timeR2.t2) ||
-      (timeR1.t1 < timeR2.t1 && timeR1.t2 > timeR2.t2) ||
-      (timeR1.t1 > timeR2.t1 && timeR1.t2 < timeR2.t2)) {
+    (timeR1.t1 > timeR2.t1 && timeR1.t1 < timeR2.t2) ||
+    (timeR1.t1 < timeR2.t1 && timeR1.t2 > timeR2.t2) ||
+    (timeR1.t1 > timeR2.t1 && timeR1.t2 < timeR2.t2)) {
     return true;
   }
   else {
@@ -194,17 +180,15 @@ function check(data, expectedType) {
 
 //#region replaceString
 function replaceString(sourceString, oldString, newString) {
-  if (!typeof sourceString == "string" ||
-  !(typeof oldString == "string" || typeof oldString == "number") ||
-  !(typeof newString == "string" || typeof newString == "number")) {
+  if (!typeof sourceString == "string" || !typeof oldString == "string" || !typeof newString == "string") {
     console.log("Неверные параметры");
     return false;
   }
-  
+
   var index = sourceString.search(oldString);
   if (index > -1) {
     var result = sourceString.replace(oldString, newString);
-    console.log(result);
+    return result;
   }
   else {
     console.log("Искомая строка не найдена");
